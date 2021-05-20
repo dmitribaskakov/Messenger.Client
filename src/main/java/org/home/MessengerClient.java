82f06ca2f85b525ac73a3715dec203e7d1101685
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-05-20 16:55:00 +0700

    Edit pom file

---
+++ src/main/java/org/home/MessengerClient.java
@@ -0,0 +1,8 @@
+package org.home;
+
+public class MessengerClient {
+
+    public static void main(String... args) throws Exception {
+        System.out.println( "MessengerClient / Hello World!" );
+    }
+}
